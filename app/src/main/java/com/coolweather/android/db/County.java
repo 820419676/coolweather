4b9b55c3ed3558ff49ed281fcbbc41ecb3097ce8
sunhao <11119721@bbktel.com>
2020-08-04 11:52:59 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,40 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Android Studio
+ * Package: com.coolweather.android.db
+ * User: 11119721
+ * Date: 2020/8/4
+ * Time: 11:47
+ */
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
